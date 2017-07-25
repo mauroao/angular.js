d038cdedb76e90d729b7ad27e5ce19e6277800a8
mauroao <mauro.anselmo.oliveira@gmail.com>
2017-07-25 00:18:18 -0300

    ajustes

--- back-end/NodeExpressServer/app.js
+++ back-end/NodeExpressServer/app.js
@@ -7,21 +7,21 @@ app.use(bodyParser.json());
 
 // dados:
 
-var contatos = [
-	{serial: 123456, nome: 'Pedro', telefone: '9999-8888', data: new Date(), operadora: {nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2.4}},
-	{serial: 234567, nome: 'Ana', telefone: '9999-8877', data: new Date(), operadora: {nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2.4}},
-	{serial: 891234, nome: 'Maria', telefone: '9999-8866', data: new Date(), operadora: {nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2.4}},
-	{serial: 923450, nome: 'Antonio da Silva', telefone: '9999-2888', data: new Date(), operadora: {nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2.4}},
-	{serial: 934560, nome: 'Ana da Silva', telefone: '9999-8872', data: new Date(), operadora: {nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2.4}},
-	{serial: 991230, nome: 'Rubens da Silva', telefone: '9999-8826', data: new Date(), operadora: {nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2.4}}
-];
-
 var operadoras = [
 	{nome: 'Oi', codigo: 14, categoria: 'Celular', preco: 2.0},
 	{nome: 'Tim', codigo: 15, categoria: 'Celular', preco: 2.1},
 	{nome: 'Vivo', codigo: 41, categoria: 'Celular', preco: 2.2},
 	{nome: 'GVT', codigo: 25, categoria: 'Fixo', preco: 2.3},
 	{nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2.4}
+];
+
+var contatos = [
+	{serial: 123456, nome: 'Pedro', telefone: '9999-8888', data: new Date(), operadora: operadoras[0]},
+	{serial: 234567, nome: 'Ana', telefone: '9999-8877', data: new Date(), operadora: operadoras[1]},
+	{serial: 891234, nome: 'Maria', telefone: '9999-8866', data: new Date(), operadora: operadoras[2]},
+	{serial: 923450, nome: 'Antonio da Silva', telefone: '9999-2888', data: new Date(), operadora: operadoras[3]},
+	{serial: 934560, nome: 'Ana da Silva', telefone: '9999-8872', data: new Date(), operadora: operadoras[4]},
+	{serial: 991230, nome: 'Rubens da Silva', telefone: '9999-8826', data: new Date(), operadora: operadoras[0]}
 ];
 
 // allow origin:
